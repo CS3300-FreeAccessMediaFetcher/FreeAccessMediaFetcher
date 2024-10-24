29eea6acae1d22c4dad6852ea8be1dd86b14712d
William <57233581+Wransom-cell@users.noreply.github.com>
2024-10-24 15:45:13 -0600

    Update to web_scraper

--- famf/scraper_components/web_scraper.py
+++ famf/scraper_components/web_scraper.py
@@ -2,17 +2,29 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
+# headers = {'User-Agent':
+#             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+#             'Accept-Language': 'en-US, en;q=0.5'}
+# headers = {'accept':'*/*',
+# 'accept-encoding':'gzip, deflate, br',
+# 'accept-language':'en-GB,en;q=0.9,en-US;q=0.8,hi;q=0.7,la;q=0.6',
+# 'cache-control':'no-cache',
+# 'dnt':'1',
+# 'pragma':'no-cache',
+# 'referer':'https',
+# 'sec-fetch-mode':'no-cors',
+# 'sec-fetch-site':'cross-site',
+# 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',}
+
 # Global Variables
-headers = {'User-Agent':
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-            'Accept-Language': 'en-US, en;q=0.5'}
+
 textCounter = 0
 linkCounter = 0
 imageCounter = 0
+videoCounter = 0
 otherCounter = 0
 dataSet = {}
 dictIndex = 0
-validDataTypes = ['web_page', 'text', 'image', 'audio', 'video']
 
 
 class siteObject:
@@ -22,7 +34,7 @@ class siteObject:
         self.dataSize = dataSize
         self.data = data
 
-
+        
 # INPUT FROM FLASK #
 def webScraperInput(url: str, data_type: str):
     print("Hello Flask!")
@@ -30,9 +42,13 @@ def webScraperInput(url: str, data_type: str):
 
 ###################
 
+def retrieveDictionary():
+    global dataSet
+    localData = dataSet
+    return localData
 
 def dataCollection(inc_website : str):
-    lobj_response = requests.get(inc_website,headers=headers)
+    lobj_response = requests.get(inc_website)
     if lobj_response.status_code == 200:
         print("Connected to " + inc_website)
         site = BeautifulSoup(lobj_response.text,"html.parser")
@@ -42,12 +58,13 @@ def dataCollection(inc_website : str):
             header.decompose()
         if footer:
             footer.decompose()
-        # print(site.prettify())
+        print(site.prettify())
         print("----------------------------------------------------------------------------------\nBeginning Harvest of site...\n----------------------------------------------------------------------------------")
 
         collectAllStrings(site)
         collectAllLinks(site)
         collectAllImg(site)
+        # collectAllVideos(site)
     else:
         print("unable to reach site")
 
@@ -75,24 +92,40 @@ def collectAllImg(inc_site: BeautifulSoup):
         addToDictionary(dataPoint)
         
 
-
+def collectAllVideos(inc_site: BeautifulSoup):
+    global videoCounter
+    #Below is a counter to label videos
+    videoNumber = 1
+    
+    video_tags = inc_site.findAll('video')
+    print("Total ", len(video_tags), "videos found")
+    for videos in inc_site.find_all('video'):
+        videoSource = videos.attrs['src']
+        videoCounter += 1
+        videoNumber += 1
+        print(videoSource)
 
 def collectAllStrings(inc_site: BeautifulSoup):
     global textCounter
     for text in inc_site.find_all(['p','title']):
         for child in text.contents:
-            dataPoint = siteObject("text","text","0mb",child.get_text())
-            textCounter += 1
-            addToDictionary(dataPoint)
+            if child.get_text() == "":
+                print("Blank text")
+            else:    
+                dataPoint = siteObject("text","text","0mb",child.get_text())
+                textCounter += 1
+                addToDictionary(dataPoint)
             
 def collectAllLinks(inc_site: BeautifulSoup):
     global linkCounter
     for atags in inc_site.find_all(['a']):
+        if atags.get_text() != "":
+            linkName = atags.get_text()
+        else:
+            linkName = "Unlabeled Link"
         link = atags.get("href")
-        if 'http' not in link:
-            link = scraping_site + "/" + link
         # if "http" in link:
-        dataPoint = siteObject("link","link","0mb",link)
+        dataPoint = siteObject("link",linkName,"0mb",link)
         linkCounter += 1
         addToDictionary(dataPoint)
 
@@ -115,26 +148,29 @@ def retrieveDataByType(inc_dataType):
 
 def retrieveDataByName(inc_dataName):
     global dataSet
-    imgFound = False
     for dataIndex in dataSet:
         if dataSet[dataIndex].dataName == inc_dataName:
             instance = dataSet[dataIndex]
-            imgFound = True
-
-    if imgFound:
-        print(instance.dataName, " : ", instance.data)
-    else:
-        print("Image not Found")
-
-# scraping_site = "https://en.wikipedia.org/wiki/List_of_Testudines_families"
-scraping_site = "https://books.toscrape.com"
-# scraping_site = "http://quotes.toscrape.com"
+            print(instance.dataName, " : ", instance.data)
 
 
-#This is solely for testing
-'''
+
+
+#The following code is solely for testing
+
+# scraping_site = "https://en.wikipedia.org/wiki/List_of_Testudines_families" #mass testing
+scraping_site = "https://books.toscrape.com" #Image testing
+# scraping_site = "https://www.youtube.com/watch?v=LJHQXmOpkUE" #video testing
+# scraping_site = "http://quotes.toscrape.com" #Text testing
+
 dataCollection(scraping_site)
+
+
 print("",textCounter, "blobs of text collected\n",linkCounter,"links collected\n",imageCounter,"images collected\n",otherCounter,"bits of unkown data collected")
+
+
+
+print(dataSet)
 
 retrievalType = input("Enter if you want type or name:")
 
@@ -147,6 +183,3 @@ elif retrievalType == "name":
     retrieveDataByName(retrieveDataTest)
 else:
     print("Invalid choice")
-'''
-
-
